fdf6ae574098eefe683d79ad657b23a3fa71692c
newsea-NGDproject <s.take.tuyopon@gmail.com>
2026-06-08 02:44:37 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'norirun-v3';
+const ASSETS = [
+  'index.html',
+  'dbclr.html',
+  'c011.html',
+  'c012.html',
+  'click.wav'
+];
+
+// インストール時にファイルをキャッシュに保存するぜ
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('アセットをキャッシュ中...');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 起動時はキャッシュから最速で読み込んで、オフラインでも動かすぜ
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
