3cd9bc919ba88075406725aeb7eee95c9451f10f
newsea-NGDproject <s.take.tuyopon@gmail.com>
2026-08-17 00:09:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'norirun-v117';
+const CACHE_NAME = 'norirun-v118';
 
 const ASSETS = [
   'index.html',
@@ -43,6 +43,7 @@ const ASSETS = [
   'js/player.js',
   'js/queue.js',
   'js/drag-sort.js',
+  'js/jump.js',
   'js/lamp.js',
   'js/metadata.js',
   'js/main.js'
